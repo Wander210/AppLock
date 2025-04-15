b228ac5324e7f4ae5f61ba144bdcc8e6c4dd9e4e
Wander210 <nthn0289@gmail.com>
2025-04-15 19:05:06 +0700

    SettingActivity 2

--- app/src/main/java/com/giang/applock20/screen/setting/SettingActivity.kt
+++ app/src/main/java/com/giang/applock20/screen/setting/SettingActivity.kt
@@ -18,7 +18,68 @@ class SettingActivity : BaseActivity<ActivitySettingBinding>() {
     }
 
     override fun handleEvent() {
+        binding.apply {
+            imgToggle1.setOnClickListener {
+                imgToggle1.startAnim {
 
+                }
+            }
+
+            imgToggle2.setOnClickListener {
+                imgToggle2.startAnim {
+
+                }
+            }
+            imgToggle3.setOnClickListener {
+                imgToggle3.startAnim {
+
+                }
+            }
+            imgToggle4.setOnClickListener {
+                imgToggle4.startAnim {
+
+                }
+            }
+            imgToggle5.setOnClickListener {
+                imgToggle5.startAnim {
+
+                }
+            }
+            imgToggle6.setOnClickListener {
+                imgToggle6.startAnim {
+
+                }
+            }
+            imgChevron1.setOnClickListener {
+                imgChevron1.startAnim {
+
+                }
+            }
+            imgChevron2.setOnClickListener {
+                imgChevron2.startAnim {
+
+                }
+            }
+            imgChevron3.setOnClickListener {
+                imgChevron3.startAnim {
+
+                }
+            }
+            imgChevron4.setOnClickListener {
+                imgChevron4.startAnim {
+
+                }
+            }
+            imgChevron5.setOnClickListener {
+                imgChevron5.startAnim {
+
+                }
+            }
+            imgChevron6.setOnClickListener {
+                imgChevron6.startAnim {
+
+                }
+            }
+        }
     }
-
 }
